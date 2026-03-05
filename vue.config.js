b7a9523a5e87823d59d97be7317ef0cc5cf84008
Eyal Shahar <eyal.person.shahar@gmail.com>
2026-03-04 23:14:47 -0800

    fixed output for vuie.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "public_html"),
+  outputDir: path.resolve(__dirname, "dist"), // public_html for ML server
   //assetsDir: "../../static/SPA"
   publicPath: process.env.NODE_ENV === 'production'
     ? '/website2021/' // was /website/
