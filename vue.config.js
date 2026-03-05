9667407a71a7a4bd23ff00146a28898d633bcc96
Eyal Shahar <eyal.person.shahar@gmail.com>
2026-03-04 21:15:07 -0800

    setting up for github pages

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,7 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "public_html"),
   //assetsDir: "../../static/SPA"
-  publicPath: "/website/"
-}
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/website2021/' // was /website/
+    : '/'
+};
